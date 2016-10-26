e86be04ff7e32a3e86aa2195501a8f255d28b057
Tom Grootjans <tom.grootjans@gmail.com>
2016-10-26 12:57:41 +0200

    layout + sidebar + toolbar

--- js/MyOnLoad.js
+++ js/MyOnLoad.js
@@ -1,18 +1,3 @@
-// function resize(){
-//     var scr = $( window ).width();
-//     var width = $('.item').width();
-//     if (width < 99){
-//         width = scr * (width / 100);
-//     }
-//
-//     $('.item-head').height(width);
-//     $('.item-head').css("padding-top", width/2 - ;
-//     console.log("width: "+width);
-//
-// };
-//
-// resize();
-//
-// $( window ).resize(function() {
-//     resize();
-// });
+setTimeout(function(){
+
+}, 500);
