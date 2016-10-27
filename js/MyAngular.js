c275a77e973efaeb88100d563a190550a8c1c67c
Tom Grootjans <tom.grootjans@gmail.com>
2016-10-27 11:55:36 +0200

    angular routes usage and items now in card nog in dialog

--- js/MyAngular.js
+++ js/MyAngular.js
@@ -2,16 +2,45 @@
     "use strict";
 
     angular
-        .module('toolsApp', ['ngMaterial'])
+        .module('toolsApp', ['ngMaterial', "ngRoute"])
         .controller('myController', AppController)
-        .controller('hourcounterController', hourcounterController);
+        .controller('hourcounterController', hourcounterController)
+        .config(['$routeProvider', RouteController]);
+
+    function RouteController($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "all.html"
+            })
+            .when("/favorite", {
+                templateUrl: "favorite.html"
+            })
+            .when("/all", {
+                templateUrl: "all.html"
+            })
+            .when("/settings", {
+                templateUrl: "settings.html"
+            })
+            .when("/item/hourcounter", {
+                templateUrl: "items/hourcounter.html"
+            })
+            .when("/settings", {
+                templateUrl: "settings.html"
+            })
+            .when("/settings", {
+                templateUrl: "settings.html"
+            });
+    }
 
     function AppController($scope, $mdDialog, $timeout, $mdSidenav) {
 
         $scope.showDialog = showDialog;
+        $scope.goBack = function() {
+            window.history.back();
+        };
         $scope.sidebarMenu = sidebarMenu;
         $scope.currentContent = 'all';
-        $scope.items = filePath(items);
+        $scope.items = items;
         $scope.toggleSidebar = function() {
             $mdSidenav('sidebar').toggle();
         }
@@ -51,119 +80,6 @@
         $scope.worked = worked;
     }
 })(angular);
-
-var sidebarMenu = [{
-    title: "Favorite",
-    desc: "Wacht all your favored items",
-    svg: "favorite",
-    link: "favorite"
-}, {
-    title: "All Items",
-    desc: "Browse all the available items",
-    svg: "dashboard",
-    link: "all"
-}, {
-    title: "Settings",
-    desc: "Wacht all your favorited programs",
-    svg: "settings-gear",
-    link: "myitems"
-}, {
-    title: "My Account",
-    desc: "Wacht all your favorited programs",
-    svg: "account-circle",
-    link: "myitems"
-}];
-
-var items = [{
-    tag: "HC",
-    name: "Hour Counter",
-    filename: "hourcounter",
-    color: "red"
-}, {
-    tag: "T2",
-    name: "Test 2",
-    filename: "test2",
-    color: "pink"
-}, {
-    tag: "T3",
-    name: "Test 3",
-    filename: "test3",
-    color: "purple"
-}, {
-    tag: "T4",
-    name: "Test 4",
-    filename: "test4",
-    color: "yellow"
-}, {
-    tag: "T5",
-    name: "Test 5",
-    filename: "test5",
-    color: "lime"
-}, {
-    tag: "T6",
-    name: "Test 6",
-    filename: "test6",
-    color: "orange"
-}, {
-    tag: "T7",
-    name: "Test 7",
-    filename: "test7",
-    color: "blue"
-}, {
-    tag: "T8",
-    name: "Test 8",
-    filename: "test8",
-    color: "cyan"
-}, {
-    tag: "T9",
-    name: "Test 9",
-    filename: "test9",
-    color: "teal"
-}, ];
-
-var worked = [{
-    day: "Maandag",
-    date: "7 juni \'16",
-    start: "09:30",
-    end: "17:30",
-    pauze: true
-}, {
-    day: "Dinsdag",
-    date: "26 oktober \'16",
-    start: "09:30",
-    end: "17:30",
-    pauze: false
-}, {
-    day: "Woensdag",
-    date: "31 december \'16",
-    start: "09:30",
-    end: "17:30",
-    pauze: true
-}, {
-    day: "Donderdag",
-    date: "1 januari \'16",
-    start: "16:45",
-    end: "21:00",
-    pauze: false
-}, {
-    day: "Vrijdag",
-    date: "31 februari \'16",
-    start: "12:00",
-    end: "17:00",
-    pauze: true
-}, {
-    day: "Zaterdag",
-    date: "14 maart \'16",
-    start: "09:30",
-    end: "17:30",
-    pauze: true
-}, {
-    day: "Zondag",
-    date: "1 april \'16",
-    start: "99:99",
-    end: "99:99",
-    pauze: false
-}];
 
 function filePath(items) {
     for (var i = 0; i < items.length; i++) {
---
+++ js/collections.js
@@ -0,0 +1,113 @@
+/* collecitons */
+var sidebarMenu = [{
+    title: "Favorite",
+    desc: "Wacht all your favored items",
+    svg: "favorite",
+    link: "favorite"
+}, {
+    title: "All Items",
+    desc: "Browse all the available items",
+    svg: "dashboard",
+    link: "all"
+}, {
+    title: "Settings",
+    desc: "Wacht all your favorited programs",
+    svg: "settings-gear",
+    link: "myitems"
+}, {
+    title: "My Account",
+    desc: "Wacht all your favorited programs",
+    svg: "account-circle",
+    link: "myitems"
+}];
+
+var items = [{
+    tag: "HC",
+    name: "Hour Counter",
+    filename: "hourcounter",
+    color: "red"
+}, {
+    tag: "T2",
+    name: "Test 2",
+    filename: "test2",
+    color: "pink"
+}, {
+    tag: "T3",
+    name: "Test 3",
+    filename: "test3",
+    color: "purple"
+}, {
+    tag: "T4",
+    name: "Test 4",
+    filename: "test4",
+    color: "yellow"
+}, {
+    tag: "T5",
+    name: "Test 5",
+    filename: "test5",
+    color: "lime"
+}, {
+    tag: "T6",
+    name: "Test 6",
+    filename: "test6",
+    color: "orange"
+}, {
+    tag: "T7",
+    name: "Test 7",
+    filename: "test7",
+    color: "blue"
+}, {
+    tag: "T8",
+    name: "Test 8",
+    filename: "test8",
+    color: "cyan"
+}, {
+    tag: "T9",
+    name: "Test 9",
+    filename: "test9",
+    color: "teal"
+}, ];
+
+var worked = [{
+    day: "Maandag",
+    date: "7 juni \'16",
+    start: "09:30",
+    end: "17:30",
+    pauze: true
+}, {
+    day: "Dinsdag",
+    date: "26 oktober \'16",
+    start: "09:30",
+    end: "17:30",
+    pauze: false
+}, {
+    day: "Woensdag",
+    date: "31 december \'16",
+    start: "09:30",
+    end: "17:30",
+    pauze: true
+}, {
+    day: "Donderdag",
+    date: "1 januari \'16",
+    start: "16:45",
+    end: "21:00",
+    pauze: false
+}, {
+    day: "Vrijdag",
+    date: "31 februari \'16",
+    start: "12:00",
+    end: "17:00",
+    pauze: true
+}, {
+    day: "Zaterdag",
+    date: "14 maart \'16",
+    start: "09:30",
+    end: "17:30",
+    pauze: true
+}, {
+    day: "Zondag",
+    date: "1 april \'16",
+    start: "99:99",
+    end: "99:99",
+    pauze: false
+}];
