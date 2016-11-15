5e923033fc26c2d74da3931d563e5925f73696d8
Tom Grootjans <tom.grootjans@gmail.com>
2016-11-15 22:21:58 +0100

    Added bool for online version, could be cleaner though but this easy AF! ;)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-var mainURL = "http://127.0.0.1/edsa-Tools.TomGrootjans.nl";
+/****** Change ****/
+var onServer = true;
+/******************/
+var mainURL = (!onServer)? "http://127.0.0.1/edsa-Tools.TomGrootjans.nl": "";
 var sidebarURL = mainURL + "/data/sidebar.json";
 var tilesURL = mainURL + "/data/tiles.json"
 var tempURL = mainURL + "/data/tempworked.json";
